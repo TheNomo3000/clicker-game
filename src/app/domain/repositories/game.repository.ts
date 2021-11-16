55768215396202b0cd45a565f4d55df003e14551
Rodrigo <rodizz@hotmail.com>
2021-11-16 11:52:08 +0100

    Add repository and localstorage repository

---
+++ src/app/domain/repositories/game.repository.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs';
+import { GameModel } from '../models/game.model';
+import { GameService } from '../../application/services/game.service';
+import { ItemType } from '../models/items-type.enum';
+export abstract class GameRepository {
+  protected game!: GameModel;
+
+  constructor(protected gameService: GameService) {
+    this.gameService.game$.subscribe({
+      next: (game) => this.game = game
+    });
+  }
+  abstract loadGame(name: string): Observable<GameModel>;
+  abstract updateClicks(clicks: number): Observable<GameModel>;
+  abstract updateItems(item: ItemType): Observable<GameModel>;
+}
---
+++ src/app/infrastructure/localstorage/game-localstorage.repository.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { GameRepository } from '../../domain/repositories/game.repository';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { GameModel } from 'src/app/domain/models/game.model';
+import { GameType } from 'src/app/domain/models/game-type.enum';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { GameService } from '../../application/services/game.service';
+import { ItemType } from '../../domain/models/items-type.enum';
+import { CONFIG } from 'src/app/config';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class GameLocalstorageRepository extends GameRepository {
+  constructor(gameService: GameService, private storage: StorageMap) {
+    super(gameService);
+  }
+
+  loadGame(name: string): Observable<GameModel> {
+    return this.checkIsGameAlreadyExists(name).pipe(
+      switchMap((game) => {
+        if (game && game.name) {
+          game.type = GameType.LOAD;
+          return of<GameModel>(game);
+        } else {
+          const newGame: GameModel = {
+            clicks: 0,
+            name,
+            type: GameType.NEW,
+            items: {
+              autoclicker: 0
+            }
+          }
+          return this.storage.set(name, newGame ).pipe(
+            map(() => newGame)
+          )
+        }
+      })
+    )
+  }
+
+  updateClicks(clicks: number): Observable<GameModel> {
+    this.game.clicks += clicks;
+    return this.updateStorage(this.game);
+  }
+
+  updateItems(item: ItemType): Observable<GameModel> {
+    this.game.clicks = this.game.clicks - (CONFIG.price[item] * this.game.items[item]);
+    this.game.items[item] += 1;
+    return this.updateStorage(this.game);
+  }
+
+  private checkIsGameAlreadyExists(name: string): Observable<GameModel> {
+    return this.storage.get(name).pipe(
+      map((game) => game as any as GameModel)
+    )
+  }
+
+  private updateStorage(game: GameModel): Observable<GameModel> {
+    return this.storage.set(this.game.name, game).pipe(
+      map(() => {
+        this.gameService.updateCurrentGame(this.game);
+        return this.game;
+      })
+    )
+  }
+}
